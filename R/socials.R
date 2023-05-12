529d3eb8afe7c2bfba95285924b7cff2917e142b
szeller42 <s.zeller@posteo.net>
2023-05-12 11:43:08 +0200

    ✨ add socials

---
+++ R/socials.R
@@ -0,0 +1,19 @@
+#' Sarah's socials
+#'
+#' Includes my socials for publishing images
+#'
+#' @importFrom dplyr mutate
+#' @import assertthat
+#'
+#' @export
+#' @return A HTML character vector
+#'
+#' @examples
+#' ggplot() +
+#'   labs(caption = socials) +
+#'   theme(plot.caption = element_textbox_simple())
+
+socials <- paste0("<span style='font-family:fa;'>&#xf09b; </span>",
+                  "szeller42",
+                  "<span style='font-family:fa;'>&#f099; </span>",
+                  "szeller42")
